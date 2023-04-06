ca60378a5e905222c016fc52c8b99fe6229dfe30
Bob Ziroll <elderziroll@gmail.com>
2023-04-06 14:18:23 -0600

    Rename worked, but need to fix the ampersands

--- rename.js
+++ rename.js
@@ -1,19 +1,29 @@
 const fs = require('fs');
 const path = require('path');
 
-const rootDir = path.join(__dirname);
-const nestedDirs = fs.readdirSync(rootDir).filter(name => !name.startsWith('.'));
+const rootDir = process.cwd();
 
-for (const nestedDir of nestedDirs) {
-  const projectDirs = fs.readdirSync(path.join(rootDir, nestedDir)).filter(name => !name.startsWith('.'));
-  for (const projectDir of projectDirs) {
-    const packageJsonPath = path.join(rootDir, nestedDir, projectDir, 'package.json');
-    if (fs.existsSync(packageJsonPath)) {
-      const packageJson = require(packageJsonPath);
-      const packageName = path.basename(projectDir).replace(/ /g, '-').toLowerCase();
-      packageJson.name = packageName;
+function processFolder(folderPath) {
+  const packageJsonPath = path.join(folderPath, 'package.json');
+  if (fs.existsSync(packageJsonPath)) {
+    const packageJson = require(packageJsonPath);
+    if (packageJson.name) {
+      packageJson.name = packageJson.name.replace(/&/g, 'and');
       fs.writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 2));
-      console.log(`Renamed package in ${path.join(nestedDir, projectDir)}/package.json to ${packageName}`);
+      console.log(`Updated ${packageJsonPath}`);
     }
+  } else {
+    const files = fs.readdirSync(folderPath);
+    files.forEach((file) => {
+      const filePath = path.join(folderPath, file);
+      const stat = fs.statSync(filePath);
+      if (stat.isDirectory()) {
+        processFolder(filePath);
+      }
+    });
   }
 }
+
+processFolder(path.join(rootDir, 'src'));
+
+console.log('Finished updating package.json files.');
